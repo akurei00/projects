ea96560bee1fa9299cc8ad400d17467ab76ec651
akurei00 <36120754+akurei00@users.noreply.github.com>
2018-02-04 13:00:39 -0500

    Base Directory Setup

---
+++ BudgetBuilder/index.php
@@ -0,0 +1,15 @@
+<?php
+	?>
+<html>
+	<head>
+		<title>Budget Builder</title>
+		<h1>Budget Builder</h1>
+	</head>
+	<body>
+		<div>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lobortis metus orci, nec vehicula ex pharetra id. Fusce eu efficitur est. Suspendisse nisi eros, consequat id dolor vitae, malesuada placerat ex. Phasellus suscipit, tortor id cursus porttitor, eros est commodo neque, sit amet laoreet magna urna a nunc. Nullam sit amet arcu nec dui mollis vehicula eu in purus. Nulla maximus porta mollis. Praesent ac justo ac justo fringilla volutpat. Phasellus in nisi sapien. Donec rutrum lectus ex, in placerat urna aliquam ut. Aliquam auctor ex sit amet ornare pharetra.<br />
+			<br />
+			Etiam quis mi nec eros bibendum iaculis sed at erat. Curabitur tempus quis erat et ultricies. Nam sed pellentesque nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur iaculis mollis sem, et iaculis est venenatis eget. Vivamus iaculis fringilla diam et lacinia. Donec nec risus quam. Curabitur tincidunt cursus lectus, sed blandit purus vestibulum at. Proin ligula ex, vehicula vitae placerat quis, aliquam eget justo. Vivamus vehicula dapibus massa.
+		</div>
+	</body>
+</html>
